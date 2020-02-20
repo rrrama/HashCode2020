6b793d6e07bc70d00e7a783f76b83cbc2f031e7f
Sam Whitehead <spw520@york.ac.uk>
2020-02-20 19:34:03 +0000

    git push git push

--- Solver.py
+++ Solver.py
@@ -16,8 +16,8 @@ class Solver:
             #fuckin libs
             LibObjects = []
             for i in range(libraries):
-                booksPerLib, signup, booksPerDay = self.splitInts(inp.realine())
-                bookTypes = self.splitInts(inp.realine())
+                booksPerLib, signup, booksPerDay = self.splitInts(inpFile.readline())
+                bookTypes = self.splitInts(inpFile.readline())
 
                 LibObjects.append(Library(i,bookTypes,signup,booksPerDay))
 
@@ -31,14 +31,15 @@ class Solver:
 
     def writeToFile(self,filename,data):
         with open(filename,"w") as outFile:
-            outFile.write(str(len(data))+"\n")
+            outFile.write(str(len(data["libOrder"]))+"\n")
             out = ""
-            for lib in data:
-                out+=lib.id + " "
+            for lib in data["libOrder"]:
+                out+=str(lib.id) + " "
             outFile.write(out+"\n")
             for i in range(len(data["libOrder"])):
                 out = ""
                 out+=str(data["libOrder"][i].id) + " "
+
                 out+=str(len(data["libBooks"][i])) + "\n"
                 outFile.write(out)
                 out = ""
@@ -61,20 +62,18 @@ class Solver:
         return score
 
     def score(self):
-
         for setName in self.datasets:
             inputData = self.readFromFile(setName+".txt")
             output = self.solve(inputData)
             print(f"Score on data set {setName}: {self.scoreOnDataset(output)}")
-            self.writeToFile(setName+"output.txt")
-
+            self.writeToFile(setName+"output.txt",output)
 
 
     def solve(self,data):
         newLibs = sorted(data["libs"],key = lambda boi: boi.signUpTime)
         output = {}
-        output["libOrder"] = [boi.id for boi in data["libs"]]
-        output["libBooks"] = {boi.id:boi.books for boi in data["libs"]}
+        output["libOrder"] = [lib for lib in newLibs]
+        output["libBooks"] = {lib.id:lib.books for lib in data["libs"]}
         output["bookValues"] = data["bookValues"]
         return output
 
@@ -82,4 +81,4 @@ if __name__ == "__main__":
     solution = Solver()
 
     D = {"libOrder": [1,0], "libBooks": {0:[1,4],1:[0,2,3,5]},"bookValues": [1, 2, 3, 6, 5, 4]}
-    print(solution.scoreOnDataset(D))
+    solution.score()
